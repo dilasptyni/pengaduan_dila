8969e2fa1a8ea555766e14a0dc80386a53eea7c3
dilasptyni <127265081+dilasptyni@users.noreply.github.com>
2023-03-15 11:25:41 +0700

    Add files via upload

---
+++ proseslogin.php
@@ -0,0 +1,18 @@
+
+<div class="card" style="padding: 42px; width: 50%; margin: 0 auto; margin-top: 10%;">
+<h3 style="text-align: center;" class="grey-text">Masuk</h3>
+<br>
+	<form method="POST" action="aksi_login.php">
+		<div class="input_field">
+			<label for="username">Username</label>
+			<input id="username" type="text" name="username" required>
+		</div>
+		<div class="input_field">
+			<label for="password">Password</label>
+			<input id="password" type="password" name="password" required>
+		</div>
+		<input type="submit" name="login" value="Masuk" class="btn grey" style="width: 100%; ">
+		<br></br>
+		<center>Belum Punya Akun? <a href="register.php">Daftar</a></center>
+	</form>
+</div>
